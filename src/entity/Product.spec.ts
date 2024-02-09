043f69529b6198be45c0d49ee62fae3fd4aec7c4
Igo B. Monteiro <igobmonteiro@gmail.com>
2024-02-09 09:11:44 -0300

    feat: add product class and product tests

---
+++ src/entity/Product.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from "vitest";
+import OrderItem from "./OrderItem";
+import Order from "./Order";
+import Product from "./Product";
+
+describe("Product unit tests", () => {
+	it("should throw error when error id is empty", () => {
+		expect(() => {
+			const product = new Product("", "Product 1", 100);
+		}).toThrowError("ID is required");
+	});
+
+	it("should throw error when error name is empty", () => {
+		expect(() => {
+			const product = new Product("123", "", 100);
+		}).toThrowError("Name is required");
+	});
+
+	it("should throw error when error price is less than 0", () => {
+		expect(() => {
+			const product = new Product("1", "123", -1);
+		}).toThrowError("Price must be greater than zero");
+	});
+
+	it("should change name", () => {
+		const product = new Product("123", "Product 1", 100);
+		product.changeName("Product 2");
+		expect(product.name).toBe("Product 2");
+	});
+
+	it("should change price", () => {
+		const product = new Product("123", "Product 1", 100);
+		product.changePrice(200);
+		expect(product.price).toBe(200);
+	});
+});
