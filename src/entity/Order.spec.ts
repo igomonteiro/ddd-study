b433ad8fa52ccdece350d160ad9fbe59fb010a7f
Igo B. Monteiro <igobmonteiro@gmail.com>
2024-02-09 09:26:46 -0300

    feat: add product and order item relation

--- src/entity/Order.spec.ts
+++ src/entity/Order.spec.ts
@@ -4,25 +4,25 @@ import Order from "./Order";
 
 describe("Order unit tests", () => {
 	it("should set _total property to the sum of prices of all OrderItems", () => {
-		const orderItem1 = new OrderItem("1", "Item 1", 10);
-		const orderItem2 = new OrderItem("2", "Item 2", 20);
+		const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+		const orderItem2 = new OrderItem("2", "Item 2", 20, "p2", 2);
 		const items = [orderItem1, orderItem2];
 		const order = new Order("1", "customer1", items);
-		expect(order.total).toBe(30);
+		expect(order.total).toBe(60);
 	});
 
 	it("should update _total property when adding an OrderItem", () => {
-		const orderItem1 = new OrderItem("1", "Item 1", 10);
-		const orderItem2 = new OrderItem("2", "Item 2", 20);
+		const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+		const orderItem2 = new OrderItem("2", "Item 2", 20, "p2", 2);
 		const items = [orderItem1];
 		const order = new Order("1", "customer1", items);
 		order.addItem(orderItem2);
-		expect(order.total).toBe(30);
+		expect(order.total).toBe(60);
 	});
 
 	it("should not throw any errors when calling validate method with valid parameters", () => {
-		const orderItem1 = new OrderItem("1", "Item 1", 10);
-		const orderItem2 = new OrderItem("2", "Item 2", 20);
+		const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+		const orderItem2 = new OrderItem("2", "Item 2", 20, "p2", 2);
 		const items = [orderItem1, orderItem2];
 		const order = new Order("1", "customer1", items);
 		expect(() => {
@@ -31,7 +31,7 @@ describe("Order unit tests", () => {
 	});
 
 	it("should throw an error when creating an Order instance with no id", () => {
-		const orderItem1 = new OrderItem("1", "Item 1", 10);
+		const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 2);
 		const items = [orderItem1];
 		expect(() => {
 			new Order("", "customer1", items);
@@ -39,7 +39,7 @@ describe("Order unit tests", () => {
 	});
 
 	it("should throw an error when creating an Order instance with no customer id", () => {
-		const orderItem1 = new OrderItem("1", "Item 1", 10);
+		const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 2);
 		const items = [orderItem1];
 		expect(() => {
 			new Order("1", "", items);
@@ -50,5 +50,13 @@ describe("Order unit tests", () => {
 		expect(() => {
 			new Order("1", "customer1", []);
 		}).toThrow("Items are required");
+	});
+
+	it("should throw an error when quantity is less or equals 0", () => {
+		expect(() => {
+			const orderItem1 = new OrderItem("1", "Item 1", 10, "p1", 0);
+			const items = [orderItem1];
+			const order = new Order("1", "customer1", items);
+		}).toThrowError("Quantity must be greater than zero");
 	});
 });
